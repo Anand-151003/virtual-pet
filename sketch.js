2353fe94af2e469c8fb47bb85ebd5579fcf64b33
Anand-151003 <74094603+Anand-151003@users.noreply.github.com>
2021-01-03 22:27:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+//Create variables here
+var dog, happyDog, database, foodS, foodStock;
+
+
+function preload()
+{
+  //load images here
+  function preload(){
+    dog= loadImage("images/Dog.png");
+    happyDog = loadImage("images/dogImg1");
+ 
+}
+}
+
+function setup() {
+	createCanvas(500, 500);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
